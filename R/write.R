9db813d76f285a3c0b01d7563878a043ee61bcaf
Simon Urbanek <simon.urbanek@r-project.org>
2011-09-04 03:29:29 +0000

    support connections in writePNG() and change default target to raw()

--- R/write.R
+++ R/write.R
@@ -1,2 +1,7 @@
-writePNG <- function(image, target)
-  invisible(.Call("write_png", image, if (is.raw(target)) target else path.expand(target), PACKAGE="png"))
+writePNG <- function(image, target = raw()) {
+  if (inherits(target, "connection")) {
+    r <- .Call("write_png", image, raw(), PACKAGE="png")
+    writeBin(r, target)
+    invisible(NULL)
+  } else invisible(.Call("write_png", image, if (is.raw(target)) target else path.expand(target), PACKAGE="png"))
+}
